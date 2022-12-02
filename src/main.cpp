87b480a4542c089f216060fd28a4365cfe744ffa
Gabriel-Karpinsky <gabriel@karpinsky.sk>
2022-12-02 09:32:31 +0100

    More signal processing updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <Arduino.h>
 #include <M5stack.h>
-/*----------------------------------------------------------------
-Branch test
-----------------------------------------------------------------*/
+
 const int pResistor = 15; //photot resistor assignedto pin 15 
 
 float value, val_old = 0, val_new = 0, time_old = 0, time_new = 1;
@@ -12,7 +10,8 @@ float freq = 0;
 float tempreading;
 float min_v = 5000, max_v = 0;
 
-int x = 0, y = 0;
+int x = 0, y = 0, x_pos = 0;
+int y_pos[320] = {239};
 
 void Clear_Screen(); //forward declaration of clear screen
 
@@ -48,12 +47,12 @@ if(val_new >= threshold && val_old <= threshold){ // if value previously was bel
   }
 
   if(x==10){
-    M5.Lcd.printf("Current_v = %10.04f\n",tempreading/20);//???
+    //M5.Lcd.printf("Current_v = %10.1f\n",tempreading/20);//???
     M5.Lcd.printf("MAX = %09.4f\n", max_v);
     M5.Lcd.printf("MIN = %09.4f\n", min_v);
-    M5.Lcd.printf("Y = %03i\n\n", y);
+    M5.Lcd.printf("Y = %03i\n", y);
     M5.Lcd.printf("FREQ = %08.4f\n", freq);
-    M5.Lcd.printf("Period = %08.4f\n", period);
+    //M5.Lcd.printf("Period = %08.4f\n", period);
     M5.Lcd.setCursor(0, 0); 
     x = 0;//reset counter 
     tempreading = 0; //reset temp value
@@ -64,14 +63,41 @@ if(val_new >= threshold && val_old <= threshold){ // if value previously was bel
 
   if(y==20){//clear screen every 20 prints
     //Clear_Screen();
-    min_v = 5000;//reset work around accidental light exposure
+    min_v = 5000;//reset min and max value counter
     max_v = 0;
     y=0;
   }
 
+  //graphing functionality
+  //~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>
+  if(x_pos == 320){
+    x_pos = 0;
+  }
+
+  M5.Lcd.drawPixel(x_pos, y_pos[x_pos], BLACK);
+
+  y_pos[x_pos] = 230 - (120 * (value / 4095));
+
+  if(value == 4095){
+    M5.Lcd.drawPixel(x_pos, y_pos[x_pos], YELLOW);
+  }
+
+  else if(value < 4095 && value >= threshold){
+    M5.Lcd.drawPixel(x_pos, y_pos[x_pos], GREEN);
+  }
+
+  else if(value < threshold){
+    M5.Lcd.drawPixel(x_pos, y_pos[x_pos], RED);
+  }
+
+
+  //~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>~<>
+
   val_old = analogRead(pResistor);
 
-  delay(5); //sampling rate of 240BPM*2*5<--(for ekg patern)
+  x_pos++;
+
+  delay(20); //sampling rate of 240BPM*2*5<--(for ekg patern)
 }
 
 void Clear_Screen(){//clear screen and set cursor back to start
